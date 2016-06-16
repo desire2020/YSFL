0c3842448ef93fe3b2ea5ffe0e7bcf0109380425
desire2020 <desire2020@126.com>
2016-06-17 00:13:25 +0800

    A basic definition for a cellular automata.

---
+++ class_cellular_automata.hpp
@@ -0,0 +1,26 @@
+#ifndef CLASS_CELLULAR_AUTOMATA
+#define CLASS_CELLULAR_AUTOMATA
+#include <string>
+using std :: string;
+const int world_size = 100;
+class cellular_automata
+{
+protected:
+	string DNA;
+public:
+	cellular_automata(const string & src);
+	void operate(int x, int y, simulated_world & context);
+};
+class simulated_world
+{
+friend class cellular_automata;
+protected:
+	int state[world_size][world_size];// 0 for dead 1 for alive(type 1)
+	int succ_state[world_size][world_size];
+public:
+	void evolute();
+	int& operator()(int x, int y);
+};
+
+
+#endif
