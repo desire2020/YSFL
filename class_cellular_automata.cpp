7da0726ae7039cab19d51fd6b22b4d01f87bad20
desire2020 <desire2020@126.com>
2016-06-17 10:43:57 +0800

    Some OOP tricks are added.

---
+++ class_cellular_automata.cpp
@@ -0,0 +1,16 @@
+#include "class_cellular_automata.hpp"
+namespace YSFL
+{
+	void cellular_automata :: operate(int x, int y, simulated_world & context)
+	{
+		
+	}
+	void simulated_world :: evolute()
+	{
+		
+	}
+	int& simulated_world :: operator()(int x, int y)
+	{
+		
+	}
+}
--- class_cellular_automata.hpp
+++ class_cellular_automata.hpp
@@ -2,25 +2,27 @@
 #define CLASS_CELLULAR_AUTOMATA
 #include <string>
 using std :: string;
-const int world_size = 100;
-class cellular_automata
+namespace YSFL
 {
-protected:
-	string DNA;
-public:
-	cellular_automata(const string & src);
-	void operate(int x, int y, simulated_world & context);
-};
-class simulated_world
-{
-friend class cellular_automata;
-protected:
-	int state[world_size][world_size];// 0 for dead 1 for alive(type 1)
-	int succ_state[world_size][world_size];
-public:
-	void evolute();
-	int& operator()(int x, int y);
-};
-
+	const int world_size = 100;
+	class cellular_automata
+	{
+	protected:
+		string DNA;
+	public:
+		cellular_automata(const string & src) : DNA(src) {}
+		void operate(int x, int y, simulated_world & context);
+	};
+	class simulated_world
+	{
+	friend class cellular_automata;
+	protected:
+		int state[world_size][world_size];// 0 for dead 1 for alive(type 1)
+		int succ_state[world_size][world_size];
+	public:
+		void evolute();
+		int& operator()(int x, int y);
+	};
+}
 
 #endif
