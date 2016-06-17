bf3c2d8fd111f8315501fe866257a638e64ba13f
desire2020 <desire2020@126.com>
2016-06-17 16:06:53 +0800

    DNA initialization.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "maindisplayport.hpp"
 #include "class_cellular_automata.hpp"
 #include <QApplication>
+using YSFL :: cellular_automata;
+using YSFL :: simulated_world;
+void initialization()
+{
+    YSFL :: all_cells.push_back(cellular_automata("if equals detect 1 3 born die)"));
+    YSFL :: all_cells.push_back(cellular_automata("if equals detect 1 2 still if equals detect 1 3 born die"));
+}
 
 int main(int argc, char *argv[])
 {
